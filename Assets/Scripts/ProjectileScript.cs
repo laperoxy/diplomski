9badc281315469cb9935fae1355a9fd6be3422df
laperoxy <peraterror@gmail.com>
2022-05-17 22:36:46 +0200

    Dodata animacija pri koliziji projektila sa terenom
    dodat zamak u pozadini
    izmenjen nivo

--- Assets/Scripts/ProjectileScript.cs
+++ Assets/Scripts/ProjectileScript.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class ProjectileScript : MonoBehaviour
 {
     public float Speed = 4.5f;
+    public Animator animator;
+    public Rigidbody2D rb;
+    private bool done;
+
+    private void Start()
+    {
+        done = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.right * Time.deltaTime * Speed;
+
+        if(!done)
+            transform.position += transform.right * Time.deltaTime * Speed;
 
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        animator.SetTrigger("Explosion");
+        done = true;
+    }
+
+    private void destroyProjectile()
     {
         Destroy(gameObject);
     }
