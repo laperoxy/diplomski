f9bddbc9aa34acb9d8d2d1b5db79b97794e15b47
valzgit <nikola.valzorio.1998@gmail.com>
2022-05-23 22:41:48 +0200

    Add initial PlayerHud network implementation

---
+++ Assets/Scripts/PlayerHud.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Netcode;
+
+public class PlayerHud: NetworkBehaviour
+{
+    private NetworkVariable<NetworkString> playersName = new NetworkVariable<NetworkString>();
+
+    private bool overlaySet = false;
+
+    public void OnConnectedToServer()
+    {
+        if (IsServer)
+        {
+            playersName.Value = $"Player {OwnerClientId}";
+        }
+    }
+}
+
+public struct NetworkString : INetworkSerializable
+{
+    private ForceNetworkSerializeByMemcpy<FixedString32Bytes> info;
+
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+    {
+        serializer.SerializeValue(ref info);
+    }
+
+    public override string ToString()
+    {
+        return info.ToString();
+    }
+
+    public static implicit operator string(NetworkString s) => s.ToString();
+    public static implicit operator NetworkString(string s) => new NetworkString() { info = new FixedString32Bytes(s) };
+}
