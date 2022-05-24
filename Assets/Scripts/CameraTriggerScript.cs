9d9755ba071488993b4b1763c7ce6fe9b230f711
valzgit <nikola.valzorio.1998@gmail.com>
2022-05-24 23:21:56 +0200

    Pillar falling down sound and camera zoom out

---
+++ Assets/Scripts/CameraTriggerScript.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Threading;
+using UnityEngine;
+
+public class CameraTriggerScript : MonoBehaviour
+{
+    [SerializeField] private float wantedOrthographicSize = 9.0f;
+    [SerializeField] private float zoomIterations = 60;
+    [SerializeField] private float zoomSmoothness = 1.1f;
+    private bool activated = false;
+    private bool shouldUpdate = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!activated)
+        {
+            shouldUpdate = true;
+            activated = true;
+        }
+    }
+    
+    private void FixedUpdate()
+    {
+        if (shouldUpdate)
+        {
+            float smoothPosition = Mathf.Lerp(Camera.main.orthographicSize, wantedOrthographicSize,
+                Time.deltaTime * zoomSmoothness);
+            Camera.main.orthographicSize = smoothPosition;
+            if (zoomIterations-- <= 0)
+            {
+                Destroy(this);
+            }
+        }
+    }
+}
