b07f4272a071e36e4c1efa615f894de52cc1d188
valzgit <nikola.valzorio.1998@gmail.com>
2022-07-13 21:29:15 +0200

    Remove Host and Client button and automatically decide who is host and who is client

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,37 +5,17 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] private Button startServerButton;
-    [SerializeField] private Button startHostButton;
-    [SerializeField] private Button startClientButton;
 
     private void Awake()
     {
         Cursor.visible = true;
     }
 
-    private void Start()
-    {
-        startHostButton.onClick.AddListener(() =>
+    private void Start() {
+        if (!NetworkManager.Singleton.StartHost())
         {
-            if (NetworkManager.Singleton.StartHost()) DisableAllButtons();
-        });
-
-        startServerButton.onClick.AddListener(() =>
-        {
-            if (NetworkManager.Singleton.StartServer()) DisableAllButtons();
-        });
-
-        startClientButton.onClick.AddListener(() =>
-        {
-            if (NetworkManager.Singleton.StartClient()) DisableAllButtons();
-        });
+            NetworkManager.Singleton.StartClient();
+        }
     }
 
-    private void DisableAllButtons()
-    {
-        startHostButton.gameObject.SetActive(false);
-        startServerButton.gameObject.SetActive(false);
-        startClientButton.gameObject.SetActive(false);
-    }
 }
