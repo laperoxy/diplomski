658b5afef06770b8f715b3a4210c68aa97fd0d29
valzgit <nikola.valzorio.1998@gmail.com>
2022-07-13 23:58:19 +0200

    Add class for server communication

---
+++ Assets/Scripts/WebPost.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WebPost: MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(PostData("petar", "password"));
+    }
+    
+    IEnumerator PostData(string username, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("username", username);
+        form.AddField("password", password);
+        
+        UnityWebRequest www = UnityWebRequest.Post("http://localhost:8080/login", form);
+
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            string results = www.downloadHandler.text;
+            
+            Debug.Log(results);
+        }
+        
+        www.Dispose();
+    }
+}
