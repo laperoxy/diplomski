771280b0af04028bc4024e6880df06b56d223cf1
valzgit <nikola.valzorio.1998@gmail.com>
2022-07-17 11:21:01 +0200

    Add classes that will be used to serialize and deserialize JSON

---
+++ Assets/Scripts/JSONClasses/LoginData.cs
@@ -0,0 +1,16 @@
+﻿namespace JSONClasses
+{ 
+    [System.Serializable]
+    public class LoginData
+    {
+        public string Username { get; set;}
+        
+        public TokenTimeResponse TokenTimeResponse { get; set;}
+
+        public LoginData(string username, TokenTimeResponse tokenTimeResponse)
+        {
+            Username = username;
+            TokenTimeResponse = tokenTimeResponse;
+        }
+    }
+}
---
+++ Assets/Scripts/JSONClasses/TokenTimeResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace JSONClasses
+{
+    [System.Serializable]
+    public class TokenTimeResponse
+    {
+        public string Token { get; set; }
+        
+        public long TimePlayed { get; set;}
+
+        public TokenTimeResponse(string token = "", long timePlayed = 0)
+        {
+            Token = token;
+            TimePlayed = timePlayed;
+        }
+    }
+}
