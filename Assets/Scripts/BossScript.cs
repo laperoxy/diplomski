0d37e726a9ddf2a2158c7fa2d3e70e8c60b86089
valzgit <nikola.valzorio.1998@gmail.com>
2022-07-24 14:24:59 +0200

    Attach necessary components to boss and add boss script

---
+++ Assets/Scripts/BossScript.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Usao u mene");
+    }
+}
