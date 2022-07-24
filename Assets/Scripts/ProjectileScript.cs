2d097c7c58f01f5f7a1b0d07ff459dd3e8e2ea26
valzgit <nikola.valzorio.1998@gmail.com>
2022-07-24 14:51:39 +0200

    Make projectile hit boss

--- Assets/Scripts/ProjectileScript.cs
+++ Assets/Scripts/ProjectileScript.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class ProjectileScript : MonoBehaviour
@@ -20,6 +21,10 @@ public class ProjectileScript : MonoBehaviour
     {
         Destroy(gameObject);
     }
-    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Detektovao trigger");
+        DestroyProjectiles();
+    }
 }
 
