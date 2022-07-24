cc1ee7c381240514aa9b36c331d436c82c3c1836
valzgit <nikola.valzorio.1998@gmail.com>
2022-07-24 16:50:31 +0200

    Remove redundant classes and imports

--- Assets/Scripts/FinishLevel.cs
+++ Assets/Scripts/FinishLevel.cs
@@ -1,17 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class FinishLevel : MonoBehaviour
 {
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
