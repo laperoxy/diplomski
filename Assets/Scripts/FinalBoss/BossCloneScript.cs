0c1555b04a9fac6e85f7ed3cdd52b043c79d2cb3
valzgit <nikola.valzorio.1998@gmail.com>
2022-07-30 16:25:46 +0200

    Add final boss phase

---
+++ Assets/Scripts/FinalBoss/BossCloneScript.cs
@@ -0,0 +1,85 @@
+using System;
+using Enums;
+using Unity.Netcode;
+using UnityEngine;
+
+public class BossCloneScript : NetworkBehaviour
+{
+
+    private readonly float MAX_BOSS_HEALTH = 100;
+    private float COOLDOWN_BETWEEN_ATTACKS = 2.5f;
+    
+    public AudioClip SoundToPlay;
+    public float Volume;
+    private AudioSource audioToPlay;
+    private DateTime lastTimeAttackWasDone = new DateTime(0);
+    [SerializeField] private Transform shotPoint;
+    [SerializeField] private GameObject fireBall;
+
+    [SerializeField] private NetworkVariable<float> networkHealthBar = new NetworkVariable<float>();
+
+    void Start()
+    {
+        audioToPlay = GetComponent<AudioSource>();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (IsServer)
+        {
+            if (col.gameObject.CompareTag("SoulPush"))
+            {
+                reduceHealth(30);
+            }
+            else if (col.gameObject.CompareTag("SoulFragment"))
+            {
+                reduceHealth(5);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (IsServer)
+        {
+            if (!IsCooldownActive())
+            {
+                AttackWithSkill();
+                lastTimeAttackWasDone = DateTime.Now;
+            }
+        }
+    }
+
+    private bool IsCooldownActive()
+    {
+        return (DateTime.Now - lastTimeAttackWasDone).TotalSeconds <= COOLDOWN_BETWEEN_ATTACKS;
+    }
+
+
+    private void AttackWithSkill()
+    {
+        Instantiate(fireBall, shotPoint.position, Quaternion.Euler(0,0,-90)).GetComponent<NetworkObject>().Spawn();
+    }
+
+    public void reduceHealth(float healthToLose)
+    {
+        
+        audioToPlay.PlayOneShot(SoundToPlay,Volume);
+        networkHealthBar.Value -= healthToLose;
+        if (networkHealthBar.Value <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    
+    private void Awake()
+    {
+        SetMaxHealth();
+    }
+
+    public void SetMaxHealth()
+    {
+        networkHealthBar.Value = MAX_BOSS_HEALTH;
+    }
+}
