251abb70dc0c0ea2473b4ac0c16d85a82ab5fd7e
valzgit <nikola.valzorio.1998@gmail.com>
2022-07-31 13:46:33 +0200

    Make some of the pillars work online

---
+++ Assets/Scripts/Environment/Vector2NormalSpeedFollower.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+public class Vector2NormalSpeedFollower : MonoBehaviour
+{
+
+    [SerializeField] private Vector2[] waypoints;
+
+    private int currentWaypointIndex = 0;
+
+    [SerializeField] private float speed = 2f;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Vector2.Distance(waypoints[currentWaypointIndex], transform.position) < .1f)
+        {
+            currentWaypointIndex++;
+            if (currentWaypointIndex >= waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex],
+            Time.deltaTime * speed);
+    }
+}
