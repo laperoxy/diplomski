fad58eece4707d28b5620200ad91005002184253
laperoxy <peraterror@gmail.com>
2022-08-03 18:33:57 +0200

    Add offline tag on top of the New Game button

--- Assets/PlayerConnectionScript.cs
+++ Assets/PlayerConnectionScript.cs
@@ -10,12 +10,15 @@ public class PlayerConnectionScript : MonoBehaviour
 {
     private readonly string SAVE_FILE_EXTENSION = "/credentials.txt";
     public GameObject on_off_button;
+    public GameObject offlineTag;
     private void Awake()
     {
         on_off_button.GetComponent<TextMeshProUGUI>().text = "OFFLINE";
+        offlineTag.SetActive(true);
         if (File.Exists(Application.dataPath + SAVE_FILE_EXTENSION))
         {
             on_off_button.GetComponent<TextMeshProUGUI>().text = "ONLINE";
+            offlineTag.SetActive(false);
         }
     }
 }
